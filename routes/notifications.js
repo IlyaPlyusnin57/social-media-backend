e924982a2e54b579ab0c84b340489718dceb52da
Ilya Plyusnin <ilyaplyusnin57@gmail.com>
2023-06-19 18:11:35 -0400

    Require the updateNotifications function
    Register the patch route for the updateNotifications function

--- routes/notifications.js
+++ routes/notifications.js
@@ -3,8 +3,10 @@ const router = require("express").Router();
 const {
   removeNotification,
   getNotifications,
+  updateNotifications,
 } = require("../controllers/notificationsController");
 
+router.patch("/update/:userId", updateNotifications);
 router.patch("/:userId", removeNotification);
 router.get("/:userId", getNotifications);
 
