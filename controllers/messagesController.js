18f1765bfef4d92266ee60ec90977bf82d659b30
Ilya Plyusnin <ilyaplyusnin57@gmail.com>
2023-06-19 17:24:46 -0400

    Remove updateNotifications
    updateNotifications is removed because the creation of the message in
    the notification object for the receiver user should only occur when the
    user is not online, so such functionality is performed in the socket
    server
    
    Also remove receiverId as it is no longer used

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -1,10 +1,6 @@
 const Message = require("../models/Message");
 const mongoose = require("mongoose");
 
-const {
-  updateNotifications,
-} = require("../controllers/notificationsController");
-
 // create a message
 
 async function createMessage(req, res) {
@@ -15,15 +11,12 @@ async function createMessage(req, res) {
     const senderId = req.body.senderId;
     const conversationId = req.body.conversationId;
     const message = req.body.message;
-    const receiverId = req.body.receiverId;
 
     const obj = await Message.create({
       senderId,
       conversationId,
       message,
     });
-
-    updateNotifications(receiverId, { message: obj._doc });
 
     res.status(200).json(obj);
   } catch (error) {
