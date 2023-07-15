85ab54d72770f4f7357455db3d3310677e253c00
Ilya Plyusnin <ilyaplyusnin57@gmail.com>
2023-07-15 18:54:22 -0400

    Create a Comment model

---
+++ models/Comment.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require("mongoose");
+
+const CommentSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    text: {
+      type: String,
+      default: "",
+      required: true,
+    },
+    postId: {
+      type: Schema.Types.ObjectId,
+      ref: "Post",
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      min: 3,
+      max: 20,
+      unique: true,
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = model("Comment", CommentSchema);
