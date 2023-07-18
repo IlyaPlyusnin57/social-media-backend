f284c27bb7b8ab4da7836143152c453cc1e08d65
Ilya Plyusnin <ilyaplyusnin57@gmail.com>
2023-07-17 20:38:08 -0400

    Import updatePostCommentCount
    Create commentObject
    
    call updatePostCommentCount for a pacticular post
    
    if user is not commenting on their own post, save commentObject in the
    db
    
    in the response, send back newComment and the commentObject so it could
    be forwarded to the socket server

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -1,12 +1,24 @@
 const Comment = require("../models/Comment");
 const CommentReply = require("../models/CommentReply");
 const mongoose = require("mongoose");
+const { updatePostCommentCount } = require("../controllers/postsController");
+const { v4: uuidv4 } = require("uuid");
+const axios = require("axios");
 
 // create a Comment
 
 async function createComment(req, res) {
-  const { type, commentBody } = req.body;
+  const { type, commentBody, postUserId, commenter } = req.body;
   let comment = null;
+
+  const commentObject = {
+    id: uuidv4(),
+    liker: commenter,
+    message: `commented on your`,
+    likedUser: postUserId,
+    type: "post",
+    typeId: commentBody.postId,
+  };
 
   try {
     if (type === "comment") {
@@ -16,7 +28,16 @@ async function createComment(req, res) {
     }
 
     const newComment = await comment.save();
-    return res.status(200).json(newComment);
+
+    await updatePostCommentCount(commentBody.postId);
+
+    if (postUserId !== commentBody.userId) {
+      await axios.patch(process.env.UPDATE_NOTIFICATIONS + postUserId, {
+        various: commentObject,
+      });
+    }
+
+    return res.status(200).json({ newComment, commentObject });
   } catch (error) {
     console.log(error);
     return res.status(500).json(error);
@@ -30,7 +51,10 @@ async function getComment(req, res) {
       : {};
 
     const comments = await Comment.find()
-      .and([{ postId: req.body.postId }, lastCommentId])
+      .and([
+        { postId: new mongoose.Types.ObjectId(req.body.postId) },
+        lastCommentId,
+      ])
       .sort({ _id: -1 })
       .limit(5)
       .lean();
