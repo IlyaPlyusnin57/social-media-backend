a2d49b9b4c5aa3d65d8046bff1ad909ede3738f8
Ilya Plyusnin <ilyaplyusnin57@gmail.com>
2023-07-28 23:55:14 -0400

    Add blocked array to User model
    User now keeps track of all the blocked users

--- models/User.js
+++ models/User.js
@@ -48,6 +48,10 @@ const UserSchema = new mongoose.Schema(
       type: Array,
       default: [],
     },
+    blocked: {
+      type: Array,
+      default: [],
+    },
     isAdmin: {
       type: Boolean,
       default: false,
